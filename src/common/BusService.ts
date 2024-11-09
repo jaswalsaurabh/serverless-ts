ae1baf355075e06d9865589116aa5c7a53670bb2
saurabh-jaswal <70649392+jaswalsaurabh@users.noreply.github.com>
2024-11-09 21:01:17 +0530

    updated query for searching the bus route
    optimised query for searching the bus route

--- src/common/BusService.ts
+++ src/common/BusService.ts
@@ -1,19 +1,66 @@
 import { Bus } from '../models/Bus';
 
-export const getBusByRoute = async (start: string, end: string) => {
+export const getBusByRoute = async (from: string, to: string) => {
+  const start = new RegExp(from, 'i');
+  const end = new RegExp(to, 'i');
+
   const buses = await Bus.aggregate([
     {
       $match: {
-        'routeInfo.placeName': { $all: [start, end] },
+        'routeInfo.placeName': {
+          $all: [start, end],
+        },
       },
     },
     {
+      // Step 2: Retrieve all the place names that matched `fromPattern` and `toPattern`
       $addFields: {
-        fromIndex: { $indexOfArray: ['$routeInfo.placeName', start] },
-        toIndex: { $indexOfArray: ['$routeInfo.placeName', end] },
+        fromIndex: {
+          $indexOfArray: [
+            '$routeInfo.placeName',
+            {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$routeInfo.placeName',
+                    cond: {
+                      $regexMatch: {
+                        input: '$$this',
+                        regex: new RegExp(from, 'i'),
+                      },
+                    },
+                  },
+                },
+                0,
+              ],
+            },
+          ],
+        },
+        toIndex: {
+          $indexOfArray: [
+            '$routeInfo.placeName',
+            {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$routeInfo.placeName',
+                    cond: {
+                      $regexMatch: {
+                        input: '$$this',
+                        regex: new RegExp(to, 'i'),
+                      },
+                    },
+                  },
+                },
+                0,
+              ],
+            },
+          ],
+        },
       },
     },
     {
+      // Step 3: Ensure fromIndex is less than toIndex for directionality
       $match: {
         $expr: { $lt: ['$fromIndex', '$toIndex'] },
       },
